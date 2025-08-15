ca0e00b5dcb16bd4cb59ff4af45a055750f1f950
Jan Kuri <jan.kuri@ngen-group.eu>
2025-08-15 11:49:25 +0200

    feat(ui): init new UI

---
+++ web/goosebit/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex min-h-svh items-center justify-center">
+      <h2 className="font-bold text-xl">Goosebit</h2>
+    </div>
+  );
+}
+
+export default App;
